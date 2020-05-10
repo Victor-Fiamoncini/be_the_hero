7ddaeed06b5188fd8749c6f195c1415b9e417b87
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-05-10 00:11:18 -0300

    other configs

--- server/src/app/database/migrations/20200509234347_create_ongs.ts
+++ server/src/app/database/migrations/20200509234347_create_ongs.ts
@@ -1,16 +1,17 @@
 import * as Knex from 'knex'
 
 export async function up(knex: Knex): Promise<any> {
-	knex.schema.createTable('ongs', (table) => {
+	return knex.schema.createTable('ongs', (table) => {
 		table.string('id').primary()
 		table.string('name').notNullable()
 		table.string('email').notNullable()
 		table.string('whatsapp').notNullable()
 		table.string('city').notNullable()
 		table.string('uf', 2).notNullable()
+		table.timestamps(true)
 	})
 }
 
 export async function down(knex: Knex): Promise<any> {
-	knex.schema.dropTableIfExists('ongs')
+	return knex.schema.dropTableIfExists('ongs')
 }
